084b428426fc1444887d8ee54d28fd58f1cc796e
Htoo Myat <htoomyat.20399@gmail.com>
2022-03-26 23:39:42 +0630

    26.3.22

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+        { title: "Welcome Party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+        { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 }
+    ]);
+
+    return (
+      <div className="home">
+          <BlogList blogs={blogs} title={"All Bolgs!"}/>
+          <BlogList blogs={blogs.filter((blog) => (blog.author === "mario"))} title={"Mario's Bolgs!"}/>
+      </div>
+    );
+}
+
+export default Home;
