4cde6c4a4a6de3d3acbc2b26f6639dae2df7021b
Htoo Myat <htoomyat.20399@gmail.com>
2022-03-28 23:36:15 +0630

    28.3.22

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-        { title: "Welcome Party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-        { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-    const DeleteHandler = (id) => {
-        const newBlog = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlog);
-    }
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 500)
+    }, []);
 
     return (
-      <div className="home">
-          <BlogList blogs={blogs} title={"All Bolgs!"} DeleteHandler={DeleteHandler}/>
-      </div>
+        <div className="home">
+            {isPending && <div>Loading....</div>}
+            {blogs && <BlogList blogs={blogs} title={"All Bolgs!"} />}
+        </div>
     );
 }
 
